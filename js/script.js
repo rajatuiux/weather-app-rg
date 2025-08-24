40f0c82fe5457f57bc5f84706733aa9139aadc5d
rajatuiux-pro <rajat.gulati27@gmail.com>
2025-08-24 17:08:50 +0530

    added class noRecord to top and bottom content

--- js/script.js
+++ js/script.js
@@ -21,44 +21,37 @@ var month = [
   "Dec",
 ];
 
-let gradientColor = document.querySelector(".topContent");
-function changeGradient(first, second) {
-  gradientColor.style.background = `linear-gradient(180deg, ${first} 0%, ${second} 100%)`;
-}
-
 async function checkWeather(city) {
   const response = await fetch(apiUrl + city + `&appid=${apiKey}&units=metric`);
+  var data = await response.json();
+
+  const utc = new Date().getTime() + new Date().getTimezoneOffset() * 60000; // world clock
+  const cityTime = new Date(utc + data.timezone * 1000); // city’s clock
 
   if (response.status == 404) {
-    document.querySelector(".topContent").innerHTML =
-      "<p class='error'>No records found</p>";
-    document.querySelector(".bottomContent").innerHTML = "";
-    document.querySelector(".bottomContent").style.padding = "0";
+    document.querySelector("main").classList.add("noRecord");
   } else {
-    var data = await response.json();
+    document.querySelector("main.noRecord").classList.remove("noRecord");
+  }
+  //   current date and time
+  document.querySelector(".dateTime").innerHTML = `${
+    weekDay[cityTime.getDay() === 0 ? 6 : cityTime.getDay() - 1]
+  }, ${month[cityTime.getMonth()]} ${cityTime.getDate()}, ${cityTime
+    .getHours()
+    .toString()
+    .padStart(2, "0")}:${cityTime.getMinutes().toString().padStart(2, "0")}`;
 
-    const utc = new Date().getTime() + new Date().getTimezoneOffset() * 60000; // world clock
-    const cityTime = new Date(utc + data.timezone * 1000); // city’s clock
+  document.querySelector(".bottomContent").style.padding = "32px 24px";
 
-    //   current date and time
-    document.querySelector(".dateTime").innerHTML = `${
-      weekDay[cityTime.getDay() === 0 ? 6 : cityTime.getDay() - 1]
-    }, ${month[cityTime.getMonth()]} ${cityTime.getDate()}, ${cityTime
-      .getHours()
-      .toString()
-      .padStart(2, "0")}:${cityTime.getMinutes().toString().padStart(2, "0")}`;
-
-    document.querySelector(".bottomContent").style.padding = "32px 24px";
-
-    document.querySelector(".city").innerHTML = data.name;
-    document.querySelector(".temp").innerHTML = `<span>${Math.round(
-      data.main.temp
-    )}</span>°C`;
-    document.querySelector(".topContent .weather").innerHTML =
-      data.weather[0].description;
-    // document.querySelector(".bottomContent .humidity").innerHTML = data.main.temp;
-    // document.querySelector(".bottomContent .wind").innerHTML = data.wind.speed;
-    document.querySelector(".humidityWind").innerHTML = `
+  document.querySelector(".city").innerHTML = data.name;
+  document.querySelector(".temp").innerHTML = `<span>${Math.round(
+    data.main.temp
+  )}</span>°C`;
+  document.querySelector(".topContent .weather").innerHTML =
+    data.weather[0].description;
+  // document.querySelector(".bottomContent .humidity").innerHTML = data.main.temp;
+  // document.querySelector(".bottomContent .wind").innerHTML = data.wind.speed;
+  document.querySelector(".humidityWind").innerHTML = `
       <li class="humidity">
         <img src="assets/humidity.png" alt="" />
         <h3><span>${data.main.temp}</span>%</h3>
@@ -71,55 +64,54 @@ async function checkWeather(city) {
       </li>
       `;
 
-    if (data.weather[0].main === "Clear") {
-      weatherIcon.src = "assets/weather/sunny.png";
-    } else if (
-      data.weather[0].main === "Rain" &&
-      500 <= data.weather[0].id <= 501
-    ) {
-      weatherIcon.src = "assets/weather/light-rain.png";
-    } else if (
-      data.weather[0].main === "Rain" &&
-      502 <= data.weather[0].id <= 504
-    ) {
-      weatherIcon.src = "assets/weather/sunny.png";
-    } else if (data.weather[0].main === "Clouds" && data.weather[0].id == 801) {
-      weatherIcon.src = "assets/weather/partly-cloudy.png";
-    } else if (
-      data.weather[0].main === "Clouds" &&
-      802 <= data.weather[0].id <= 803
-    ) {
-      weatherIcon.src = "assets/weather/partly-cloudy-with-sun.png";
-    } else if (data.weather[0].main === "Clouds" && data.weather[0].id == 804) {
-      weatherIcon.src = "assets/weather/overcast.png";
-    } else if (data.weather[0].main === "Fog" && data.weather[0].id == 741) {
-      weatherIcon.src = "assets/weather/foggy.png";
-    } else if (
-      data.weather[0].main === "Thunderstorm" &&
-      210 <= data.weather[0].id <= 221
-    ) {
-      weatherIcon.src = "assets/weather/thunderstorm.png";
-    } else if (
-      data.weather[0].main === "Thunderstorm" &&
-      200 <= data.weather[0].id <= 202 &&
-      230 <= data.weather[0].id <= 232
-    ) {
-      weatherIcon.src = "assets/weather/thunderstorm-with-rain.png";
-    } else if (
-      data.weather[0].main === "Snow" &&
-      600 <= data.weather[0].id <= 601 &&
-      611 <= data.weather[0].id <= 621
-    ) {
-      weatherIcon.src = "assets/weather/snow.png";
-    } else if (data.main.temp > 40) {
-      weatherIcon.src = "assets/weather/hot-temperature.png";
-    } else if (data.main.temp < 10) {
-      weatherIcon.src = "assets/weather/cold-temperature.png";
-    } else if (data.weather[0].main === "Mist") {
-      weatherIcon.src = "assets/weather/windy.png";
-    } else {
-      weatherIcon.src = "assets/weather/sunny.png";
-    }
+  if (data.weather[0].main === "Clear") {
+    weatherIcon.src = "assets/weather/sunny.png";
+  } else if (
+    data.weather[0].main === "Rain" &&
+    500 <= data.weather[0].id <= 501
+  ) {
+    weatherIcon.src = "assets/weather/light-rain.png";
+  } else if (
+    data.weather[0].main === "Rain" &&
+    502 <= data.weather[0].id <= 504
+  ) {
+    weatherIcon.src = "assets/weather/sunny.png";
+  } else if (data.weather[0].main === "Clouds" && data.weather[0].id == 801) {
+    weatherIcon.src = "assets/weather/partly-cloudy.png";
+  } else if (
+    data.weather[0].main === "Clouds" &&
+    802 <= data.weather[0].id <= 803
+  ) {
+    weatherIcon.src = "assets/weather/partly-cloudy-with-sun.png";
+  } else if (data.weather[0].main === "Clouds" && data.weather[0].id == 804) {
+    weatherIcon.src = "assets/weather/overcast.png";
+  } else if (data.weather[0].main === "Fog" && data.weather[0].id == 741) {
+    weatherIcon.src = "assets/weather/foggy.png";
+  } else if (
+    data.weather[0].main === "Thunderstorm" &&
+    210 <= data.weather[0].id <= 221
+  ) {
+    weatherIcon.src = "assets/weather/thunderstorm.png";
+  } else if (
+    data.weather[0].main === "Thunderstorm" &&
+    200 <= data.weather[0].id <= 202 &&
+    230 <= data.weather[0].id <= 232
+  ) {
+    weatherIcon.src = "assets/weather/thunderstorm-with-rain.png";
+  } else if (
+    data.weather[0].main === "Snow" &&
+    600 <= data.weather[0].id <= 601 &&
+    611 <= data.weather[0].id <= 621
+  ) {
+    weatherIcon.src = "assets/weather/snow.png";
+  } else if (data.main.temp > 40) {
+    weatherIcon.src = "assets/weather/hot-temperature.png";
+  } else if (data.main.temp < 10) {
+    weatherIcon.src = "assets/weather/cold-temperature.png";
+  } else if (data.weather[0].main === "Mist") {
+    weatherIcon.src = "assets/weather/windy.png";
+  } else {
+    weatherIcon.src = "assets/weather/sunny.png";
   }
 }
 
@@ -131,7 +123,6 @@ searchBtn.addEventListener("click", (e) => {
   e.preventDefault();
   if (inputCity.value) {
     checkWeather(inputCity.value);
-    changeGradient();
   } else {
     checkWeather("Delhi");
   }
